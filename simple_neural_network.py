aa6af92dfb7b9506abae9b3c48641dc7f425cca3
MinasMayth <minasmayth@protonmail.com>
2021-02-02 12:55:13 +0100

    Added in the forward, sigmoid, sigmoidPrime and backward functions for the neural network class - will continue later

--- simple_neural_network.py
+++ simple_neural_network.py
@@ -10,6 +10,47 @@ import numpy as np
 from sklearn.preprocessing import MinMaxScaler
 import matplotlib.pyplot as plt
 
+# Here we define the neural network
+
+class NeuralNetwork():
+    def __init__(self,):
+        self.inputSize = 3
+        self.outputSize = 1
+        self.hiddenSize = 3
+        
+        self.W1 = np.random.rand(self.inputSize, self.hiddenSize)
+        self.W2 = np.random.rand(self.hiddenSize, self.outputSize)
+        
+        self.error_list = []
+        self.limit = 0.5
+        self.true_positives = 0
+        self.false_positives = 0
+        self.true_negatives = 0
+        self.false_negatives = 0
+        
+    def forward(self, X):
+        self.z = np.matmul(X, self.W1)
+        self.z2 = self.sigmoid(self.z)
+        self.z3 = np.matmul(self.z2, self.W2)
+        o = self.sigmoid(self.z3)
+        return o
+    
+    def sigmoid(self,s):
+        return 1/(1 + np.exp(-s))
+    
+    def sigmoidPrime(self,s):
+        return s*(1-s)
+    
+    def backward(self,X,y,o):
+        self.o_error = y - o
+        self.o_delta = self.o_error * self.sigmoidPrime(o)
+        self.z2_error = np.matmul(self.o_delta,
+                                  np.matrix.transpose(self.W2))
+        self.z2_delta = self.z2_error * self.sigmoidPrime(self.z2)
+        self.W1 += np.matmul(np.matrix.transpose(X), self.z2_delta)
+        self.W2 += np.matmul(np.matrix.transpose(self.z2),
+                             self.o_delta)
+
 
 """
 To make sure that the model is evaluated based on how good 
@@ -44,6 +85,3 @@ input_test_scaled = scaler.fit_transform(input_test)
 output_test_scaled = scaler.fit_transform(output_test)
 
 
-class NeuralNetwork():
-    def __init__(self,):
-        
